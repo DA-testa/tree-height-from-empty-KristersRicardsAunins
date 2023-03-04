bced70b60f411fa41aeafebbe1216aee668d5929
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-04 16:50:05 +0200

    test

--- tree_height.py
+++ tree_height.py
@@ -2,24 +2,59 @@
 
 import sys
 import threading
-from collections import defaultdict
+import numpy
     
-def compute_height(n, parents):
-    tree = defaultdict(list)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        
+def find_height(n, parent):
+    nodes = [Node(i) for i in range (n)]
+    root = None
+        
     for i in range(n):
-        if parents[i] == -1:
-            root = i
+        if parent[i] == -1:
+            root = nodes[i]
         else:
-            tree[parents[i]].append(i)
+            parent_node = nodes[parenti[i]]
+            parent_node.children.append(nodes[i])
+                
+    max_height = 0
+    queue = [(root, 1)]
     
-    def dfs(node):
-        if not tree [node]:
-            return 0
-        else:
-            return max(dfs(child) for child in tree[node]) + 1
-    
-    return dfs(root)
+    while queue:
+        node, level = queue.pop(0)
+        if level > max_height:
+            max_height = level
+        for child in node.children:
+            queue.append(child, level + 1)
+    return max_height
 
-    n = 10
-    parents = [9, 7, 5, 5, 2, 9, 9, 9, 2, -1]
-    print(compute_height(n, parents))
+def main():
+    input_type = input(""I" or "F"" )
+    if input_type == "I":
+        n = int(input())
+        parent = list(map(int, input().split()))
+        height = find_height(n, parent)
+        print(height)
+       
+    elif input_type == "F":
+        file_name = input("File name: ")
+        while 'a' in file_name:
+            print("File name cannot contain 'a'")
+            file_name = input("file name: ")
+        file_path = "./data/" + file_name
+        
+        while open(file_path) as f:
+            n = int(f.readline().strip())
+            parent = list(map(int, f.readline().strip().split()))
+            height = find_height(n, parent)
+            print(height)
+            
+    pass
+
+if __name__ == '__main__'
+    sys.setrecurionlimit(107)
+    threading.stack_size(227)
+    threading.Thread(target=main).start()
